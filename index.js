5e444df09f07bd1c514a3ce2723be548c2e3234a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 13:59:56 +0200

    url parameters

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
 const parse = require('./lib/parse').default;
 const draw = require('./lib/draw').default;
 const convertToMidi = require('./lib/convertToMidi').default;
 
-parse('./input/calibration_sheet.csv', musicData => {
-  const svg = draw(musicData);
-  const midi = convertToMidi(musicData);
-  fs.writeFileSync("./output/export.svg", svg)
-  console.log("SVG written to './output/export.svg'");
-  fs.writeFileSync("./output/midi.midi", midi)
-  console.log("MIDI written to './output/midi.midi'");
-})
+console.log("Wintergatan Programming templates converter");
+console.log("===========================================");
+if (argv._.length !== 1 || (!argv.svg && !argv.midi)) {
+  console.log("Usage: node index.js input.csv --svg output.svg --midi output.midi");
+  console.log("You can either supply svg, midi or both.");
+} else {
+  parse(argv._[0], musicData => {
+    if (argv.svg) {
+      const svg = draw(musicData);
+      fs.writeFileSync(argv.svg, svg)
+      console.log("SVG written to '" + argv.svg + "'");
+    }
+    if (argv.midi) {
+      const midi = convertToMidi(musicData);
+      fs.writeFileSync("./output/midi.midi", midi)
+      console.log("MIDI written to '" + argv.midi + "'");
+    }
+  })
+}
